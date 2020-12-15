5609fb72abf7501c64f71c57d58208ed71146c0c
Rafael Badiale <rafael.badiale@dieboldnixdorf.com>
2020-12-15 18:50:15 -0300

    Added examples for Display class

---
+++ examples/display_class.py
@@ -0,0 +1,28 @@
+import epaper
+import framebuf
+from time import sleep
+
+def main(fast=False):
+    d = epaper.Display(portrait=True, fast=fast)
+    fb = framebuf.FrameBuffer(d.image, 128, 296, framebuf.MONO_HLSB)
+    for n in range(16):
+        fb.text('Hello', n * 8, n * 8, 0)
+    for n in range(24):
+        fb.text('olleH', 128 - (n * 8), (15 + n) * 8, 0)
+    d.init()
+    d.write_image()
+    d.update()
+
+    sleep(15)
+
+    d = epaper.Display(fast=fast)
+    fb = framebuf.FrameBuffer(d.image, 296, 128, framebuf.MONO_VLSB)
+    for n in range(16):
+        fb.text('this is only a text', n * 8, n * 8, 0)
+    d.init()
+    d.write_image()
+    d.update()
+
+    sleep(15)
+
+    d.full_refresh()
